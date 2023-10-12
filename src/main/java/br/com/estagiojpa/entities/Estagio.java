0ec541f324a30e274c2146146327cf6b0e8c88b0
Davi Morais <davifarias2433@gmail.com>
2023-10-11 23:19:10 -0300

    TODO: Implementar mapeamento - Um estágio deve estar associado a apenas um Aluno, uma Empresa e um Orientador.

--- src/main/java/br/com/estagiojpa/entities/Estagio.java
+++ src/main/java/br/com/estagiojpa/entities/Estagio.java
@@ -4,7 +4,68 @@
  */
 package br.com.estagiojpa.entities;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public class Estagio {
+@Entity
+@Table(name = "estagios")
+public class Estagio implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String inicioEstagio;
+    private String fimEstagio;
+    
+    private int cargaHoraria;
+    
+    private String status; //em andamento ou finalizado
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInicioEstagio() {
+        return inicioEstagio;
+    }
+
+    public void setInicioEstagio(String inicioEstagio) {
+        this.inicioEstagio = inicioEstagio;
+    }
+
+    public String getFimEstagio() {
+        return fimEstagio;
+    }
+
+    public void setFimEstagio(String fimEstagio) {
+        this.fimEstagio = fimEstagio;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
     
 }
