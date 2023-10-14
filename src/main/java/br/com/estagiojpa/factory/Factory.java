bdc23466d0c832d1687ac6609fd2a362bdb30eea
Davi Morais <davifarias2433@gmail.com>
2023-10-14 03:12:27 -0300

    Atualizando factory para criar alunos

--- src/main/java/br/com/estagiojpa/factory/Factory.java
+++ src/main/java/br/com/estagiojpa/factory/Factory.java
@@ -4,6 +4,7 @@
  */
 package br.com.estagiojpa.factory;
 
+import br.com.estagiojpa.entities.Aluno;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -13,6 +14,26 @@ public class Factory {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("my_persistence_unit");
         EntityManager manager = factory.createEntityManager();
+        
+        //Criando os alunos Nicole e Mateus Luiz:
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Nicole");
+        aluno1.setGenero('F');
+        aluno1.setEmail("nicole.@email.com");
+        aluno1.setIdade(20);
+        
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Mateus Luiz");
+        aluno2.setGenero('M');
+        aluno2.setEmail("mateusluiz@email.com");
+        aluno2.setIdade(19);
+        
+        manager.getTransaction().begin();
+        manager.persist(aluno1);
+        manager.persist(aluno2);
+        manager.getTransaction().commit();
+        manager.close();
         
         
         factory.close();
