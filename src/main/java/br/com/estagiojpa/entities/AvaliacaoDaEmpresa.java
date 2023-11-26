7e77e7d58baa4862d5cae4e489e47a3212ca498e
Davi Morais <davifarias2433@gmail.com>
2023-11-26 20:36:44 -0300

    Tentando atualizar database

---
+++ src/main/java/br/com/estagiojpa/entities/AvaliacaoDaEmpresa.java
@@ -0,0 +1,91 @@
+package br.com.estagiojpa.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "avaliacaoEmpresa")
+public class AvaliacaoDaEmpresa implements Serializable {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	// Opcoes de resposta 
+	// Insuficiente
+    // Regular
+    // Bom
+    // Muito bom
+	
+	private String RendimentoTrabalho;
+	private String Conhecimentos;
+	private String CumprimentoTarefas;
+	private String Aprendizagem;
+	private String Desempenho;
+	
+	
+	
+	@OneToOne
+    private Empresa empresa;
+    
+	@Column(unique = true)
+    private Orientador orientador;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRendimentoTrabalho() {
+		return RendimentoTrabalho;
+	}
+	public void setRendimentoTrabalho(String rendimentoTrabalho) {
+		RendimentoTrabalho = rendimentoTrabalho;
+	}
+	public String getConhecimentos() {
+		return Conhecimentos;
+	}
+	public void setConhecimentos(String conhecimentos) {
+		Conhecimentos = conhecimentos;
+	}
+	public String getCumprimentoTarefas() {
+		return CumprimentoTarefas;
+	}
+	public void setCumprimentoTarefas(String cumprimentoTarefas) {
+		CumprimentoTarefas = cumprimentoTarefas;
+	}
+	public String getAprendizagem() {
+		return Aprendizagem;
+	}
+	public void setAprendizagem(String aprendizagem) {
+		Aprendizagem = aprendizagem;
+	}
+	public String getDesempenho() {
+		return Desempenho;
+	}
+	public void setDesempenho(String desempenho) {
+		Desempenho = desempenho;
+	}
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	public Orientador getOrientador() {
+		return orientador;
+	}
+	public void setOrientador(Orientador orientador) {
+		this.orientador = orientador;
+	}
+}
---
+++ src/main/java/br/com/estagiojpa/entities/AvaliacaoDoProfessor.java
@@ -0,0 +1,95 @@
+package br.com.estagiojpa.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "avaliacaoProfessor")
+public class AvaliacaoDoProfessor implements Serializable {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	// Opcoes de resposta 
+	// Insuficiente
+    // Regular
+    // Bom
+    // Muito bom
+	
+	private String Assiduidade;
+	private String Disciplina;
+	private String Sociabilidade;
+	private String Responsabilidade;
+	private String Iniciativa;
+	
+	
+	@OneToOne
+    private Aluno aluno;
+    
+	@Column(unique = true)
+    private Orientador orientador;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAssiduidade() {
+		return Assiduidade;
+	}
+	public void setAssiduidade(String assiduidade) {
+		Assiduidade = assiduidade;
+	}
+	public String getDisciplina() {
+		return Disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		Disciplina = disciplina;
+	}
+	public String getSociabilidade() {
+		return Sociabilidade;
+	}
+	public void setSociabilidade(String sociabilidade) {
+		Sociabilidade = sociabilidade;
+	}
+	public String getResponsabilidade() {
+		return Responsabilidade;
+	}
+	public void setResponsabilidade(String responsabilidade) {
+		Responsabilidade = responsabilidade;
+	}
+	public String getIniciativa() {
+		return Iniciativa;
+	}
+	public void setIniciativa(String iniciativa) {
+		Iniciativa = iniciativa;
+	}
+	public Aluno getAluno() {
+		return aluno;
+	}
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+	public Orientador getOrientador() {
+		return orientador;
+	}
+	public void setOrientador(Orientador orientador) {
+		this.orientador = orientador;
+	}
+	
+}
