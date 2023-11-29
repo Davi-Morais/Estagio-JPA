c91a7545ebb264e383377f79f7f45751afd90d40
Davi Morais <davifarias2433@gmail.com>
2023-11-28 21:07:05 -0300

    Criando AlunoService

---
+++ src/main/java/br/com/estagiojpa/controller/services/AlunoService.java
@@ -0,0 +1,35 @@
+package br.com.estagiojpa.controller.services;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import br.com.estagiojpa.model.daos.AlunoDAO;
+import br.com.estagiojpa.model.utils.Transactional;
+import br.com.estagiojpa.controller.exceptions.BusinessException;
+import br.com.estagiojpa.entities.Aluno;
+
+public class AlunoService implements Serializable {
+	
+	@Inject
+	private AlunoDAO alunoDAO;
+	
+	@Transactional
+	public void salvar(Aluno aluno) throws BusinessException {
+		if (aluno == null) {
+			throw new BusinessException("Não foi possível salvar a Editora.");
+		}
+		this.alunoDAO.guardar(aluno);
+	}
+	
+	
+	@Transactional
+	public void excluir(Aluno aluno) throws BusinessException {
+		aluno = this.alunoDAO.porId(aluno.getId());
+		if (aluno == null) {
+			throw new BusinessException("Não é possível excluir a Editora!");
+		}
+		this.alunoDAO.remover(aluno);
+	}
+
+}
